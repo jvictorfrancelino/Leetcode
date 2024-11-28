ec95e0b9bfc96305cdf6552d64ce27089689cdde
José Victor <jvictorfrance@outlook.com>
2024-11-27 21:02:48 -0300

    Starting 2 challenges

---
+++ MergeStringsAlternately/Main.java
@@ -0,0 +1,17 @@
+package MergeStringsAlternately;
+
+public class Main {
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        String result = MergeStringsAlternately(word1, word2);
+        System.out.println("The first string is: " + word1);
+        System.out.println("The second string is: " + word2);
+        System.out.println("The result is: " + result);
+
+    }
+
+    public static String MergeStringsAlternately(String word1, String word2){
+        return null;
+    }
+}
